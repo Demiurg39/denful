4bac915142b11b58e84e1bb2bdc324337032f479
demiurg39 <chiryagov2014@gmail.com>
2026-05-30 00:51:27 +0600

    add network modules

---
+++ modules/system/settings/network/tcp-optimization.nix
@@ -0,0 +1,17 @@
+{
+  flake.modules.nixos.tcp-optimization = {
+    boot.kernel.sysctl = {
+      ## TCP optimization
+      # TCP Fast Open is a TCP extension that reduces network latency by packing
+      # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
+      # both incoming and outgoing connections:
+      net.ipv4.tcp_fastopen = 3;
+
+      # Bufferbloat mitigations + slight improvement in throughput & latency
+      net.ipv4.tcp_congestion_control = "bbr";
+      net.core.default_qdisc = "cake";
+    };
+
+    boot.kernelModules = ["tcp_bbr"];
+  };
+}
