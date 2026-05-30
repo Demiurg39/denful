cc601123206d4a8f93816c11556e31314f1f3c1e
demiurg39 <chiryagov2014@gmail.com>
2026-05-30 16:20:16 +0600

    add generic media

---
+++ modules/programs/media/generic.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  lib,
+  ...
+}: let
+  inherit (inputs.self.lib) assoc;
+in {
+  flake.modules.homeManager.media-tools = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      loupe
+      amberol
+      nemo
+      file-roller
+    ];
+
+    xdg = {
+      mimeApps = {
+        enable = true;
+        defaultApplications = lib.mkMerge [
+          (assoc pkgs.loupe {
+            image = ["png" "avif" "svg" "jpeg" "jpg" "gif"];
+          })
+          (assoc pkgs.amberol {
+            audio = ["mp3" "flac" "wav" "aac"];
+          })
+        ];
+      };
+    };
+  };
+}
