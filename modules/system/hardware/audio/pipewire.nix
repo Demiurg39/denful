9990e024f96d2a7883d75ed9a8c79b2985427d01
demiurg39 <chiryagov2014@gmail.com>
2026-05-30 14:06:01 +0600

    add pipewire audio setup

---
+++ modules/system/hardware/audio/pipewire.nix
@@ -0,0 +1,21 @@
+{
+  flake.modules.nixos.audio.pipewire = {
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    # make pipewire realtime-capable
+    security.rtkit.enable = true;
+  };
+
+  flake.modules.homeManager.audio.pipewire = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      pavucontrol
+      pamixer
+    ];
+  };
+}
