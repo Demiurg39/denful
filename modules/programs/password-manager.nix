87f7dff8e6c74b921ab96fe77003de2858fabe6f
demiurg39 <chiryagov2014@gmail.com>
2026-06-01 01:10:33 +0600

    comented keyguard

--- modules/programs/password-manager.nix
+++ modules/programs/password-manager.nix
@@ -2,7 +2,7 @@
   flake.modules.homeManager.passwordManager = {pkgs, ...}: {
     home.packages = with pkgs; [
       # FIX: has unfree license, add unfree handling
-      keyguard
+      # keyguard
       rbw
     ];
   };
