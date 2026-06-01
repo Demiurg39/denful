31c4cdc9e43ed9bc8d0b3e90249ea048b26ce2e4
demiurg39 <chiryagov2014@gmail.com>
2026-06-01 12:35:23 +0600

    move ethernet setting to network-manager module

--- modules/system/settings/network/network-manager.nix
+++ modules/system/settings/network/network-manager.nix
@@ -1,14 +1,14 @@
 {
-  flake.modules.nixos.wifi = {
+    flake.modules.nixos.network-manager = {
+
     networking = {
       networkmanager = {
+        enable = true;
         settings = {
-          # Use a random MAC for Wi-Fi scanning
-          wifi.scan-rand-mac-address = "yes";
           # Use a stable but randomized MAC for each connection
           connection.wifi-cloned-mac-address = "stable";
         };
       };
     };
-  };
+    };
 }
