bac1fe7ff525214de880dd273f78594d8fea49c0
demiurg39 <chiryagov2014@gmail.com>
2026-06-08 15:33:45 +0600

    fix disk path and change configuration
    Set luks encryption over btrfs subvolumes

--- modules/hosts/asura/filesystem.nix
+++ modules/hosts/asura/filesystem.nix
@@ -1,41 +1,63 @@
 # TODO: move to disko config
 {
+  config,
+  self,
+  ...
+}: {
   flake.modules.nixos.asura = {
     disko.devices.disk.main = {
       type = "disk";
-      device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
+      device = "/dev/disk/by-id/nvme-eui.002538dc21a0d33e";
       content = {
         type = "gpt";
         partitions = {
           ESP = {
             priority = 1;
             name = "ESP";
-            start = "1M";
-            end = "128M";
+            size = "1024M";
             type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = ["umask=0022"];
+              mountOptions = ["umask=0077"];
             };
           };
-          root = {
+          luks = {
             size = "100%";
             content = {
-              type = "btrfs";
-              extraArgs = ["-f"];
-              subvolumes = {
-                "/rootfs" = {
-                  mountpoint = "/";
-                };
-                "/home" = {
-                  mountpoint = "/home";
-                  mountOptions = ["compress=zstd"];
-                };
-                "/nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = ["compress=zstd" "noatime"];
+              type = "luks";
+              name = "crypted";
+              settings.allowDiscards = true;
+              extraFormatArgs = ["--type luks2" "--pbkdf argon2id"];
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f" "--csum xxhash"];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "discard=async"
+                      "noatime"
+                    ];
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                      "discard=async"
+                      "noatime"
+                    ];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "discard=async"
+                      "noatime"
+                    ];
+                  };
                 };
               };
             };
@@ -48,39 +70,6 @@
     services.btrfs.autoScrub = {
       enable = true;
       interval = "monthly";
-      fileSystems = ["/"];
     };
-
-    # # TODO: add impermanence setup
-    # fileSystems."/" = {
-    #   device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
-    #   fsType = "btrfs";
-    #   options = ["subvol=@" "compress=zstd" "noatime"];
-    # };
-    #
-    # fileSystems."/home" = {
-    #   device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
-    #   fsType = "btrfs";
-    #   options = ["subvol=@home" "compress=zstd" "noatime"];
-    # };
-    #
-    # # TODO: fix this, cause its not working
-    # # fileSystems."/nix" = {
-    # #   device = "/dev/disk/by-uuid/b9eae7f6-9aab-428e-88dc-a9df3dbd72fb";
-    # #   fsType = "btrfs";
-    # #   options = ["subvol=@nix" "compress=zstd" "noatime"];
-    # # };
-    #
-    # fileSystems."/boot" = {
-    #   device = "/dev/disk/by-label/ESP";
-    #   fsType = "vfat";
-    # };
-    #
-    # swapDevices = [
-    #   {
-    #     device = "/var/swapfile";
-    #     size = 8192;
-    #   }
-    # ];
   };
 }
