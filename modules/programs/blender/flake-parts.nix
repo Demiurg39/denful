b1b0698422bb9cffaa4ef4bea2f03f0a6d8f1aa0
demiurg39 <chiryagov2014@gmail.com>
2026-06-11 19:00:08 +0600

    fix blender input string

--- modules/programs/blender/flake-parts.nix
+++ modules/programs/blender/flake-parts.nix
@@ -2,6 +2,6 @@
   flake-file.inputs = {
     # TODO: make it valid path
     # https://github.com/edolstra/nix-warez/tree/master/blender
-    # blender = "github:edolstra/nix-warez/blender?ref=blender";
+    # blender = "github:edolstra/nix-warez/blender?dir=blender";
   };
 }
