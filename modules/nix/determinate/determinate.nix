288e203f1faa2a485d1b35405349ab127dae44f8
demiurg39 <chiryagov2014@gmail.com>
2026-06-11 18:18:11 +0600

    add detsys as substituters

--- modules/nix/determinate/determinate.nix
+++ modules/nix/determinate/determinate.nix
@@ -1,5 +1,10 @@
 {inputs, ...}: {
   flake.modules.nixos.determinate = {
     imports = [inputs.determinate.nixosModules.default];
+
+    nix.settings = {
+      substituters = ["https://install.determinate.systems"];
+      trusted-public-keys = ["cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="];
+    };
   };
 }
