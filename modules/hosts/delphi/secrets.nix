353073b5a834c1fe219ff54f0a7cdfee41e9bc1e
demiurg39 <chiryagov2014@gmail.com>
2026-06-15 15:01:30 +0600

    add delphi ssh key

--- modules/hosts/delphi/secrets.nix
+++ modules/hosts/delphi/secrets.nix
@@ -1,7 +1,7 @@
 {
   flake.modules.nixos.delphi = {
-    # age.rekey = {
-    #   hostPubkey = "";
-    # };
+    age.rekey = {
+      hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELtYJEqDEGnLOOt9/pmnRAHktnz3VH2H51JAx8xziX6";
+    };
   };
 }
