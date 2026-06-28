b454fc0412779d275c017c37bf94e2c2088b8364
demiurg39 <chiryagov2014@gmail.com>
2026-06-28 16:53:33 +0600

    add usbguard rules for demi

---
+++ modules/users/demi/services/usbguard.nix
@@ -0,0 +1,9 @@
+{inputs, ...}: {
+  flake.modules.nixos.demi = {
+    imports = [inputs.self.modules.nixos.usbguard];
+    services.usbguard.rules = ''
+      allow id 1d6b:0002 name "xHCI Host Controller" hash "4a4NgfdUaJO43rkCzmWRSeHHR/uUh5+SNsXnhosm9qs=" parent-hash "ldMchY4Tt4GPUYo30eNGvai+Fs/EdnVY3vMyxJUq4Nk=" with-interface 09:00:00 with-connect-type ""
+      allow id 04e8:6864 name "SAMSUNG_Android" hash "JorMk9jefVHgGXcW80VUeHb61ECb3Kt4AgmK4U/Pq5U=" parent-hash "TaKoMrgQrk94nyzpOQk+iNVB0H+ZSnYN/X7lY+QzAn0="
+    '';
+  };
+}
